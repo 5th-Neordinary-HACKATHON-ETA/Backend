e53cd1d4da07870cf8fbfd2c2d0c53aacbf7a1af
JINHYEONG KIM <sgo5178@kyonggi.ac.kr>
2023-11-26 01:12:03 +0900

    Fix: 회의 스키마에 팀 id 컬럼 추가 팀이 존재하지 않을시 회의 생성 불가 처리

--- src/meetings/meetings.module.ts
+++ src/meetings/meetings.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Meeting } from './entities/meeting.entity';
 import { MeetingsController } from './meetings.controller';
 import { MeetingsService } from './meetings.service';
+import { Team } from '../teams/entities/team.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Meeting])],
+  imports: [TypeOrmModule.forFeature([Meeting, Team])],
   controllers: [MeetingsController],
   providers: [MeetingsService],
   exports: [],
--- src/meetings/meetings.service.ts
+++ src/meetings/meetings.service.ts
@@ -5,17 +5,36 @@ import { Meeting } from './entities/meeting.entity';
 import { CreateDto } from './dtos/request/create.dto';
 import { ResponseBody, SuccessResponse } from '../common/response/response';
 import { RESPONSE_CODE } from '../common/response/response.code';
+import { Team } from '../teams/entities/team.entity';
+import { Exception } from '../common/response/exception';
 
 @Injectable()
 export class MeetingsService {
   constructor(
     @InjectRepository(Meeting)
     private meetingsRepository: Repository<Meeting>,
+    @InjectRepository(Team)
+    private teamsRepository: Repository<Team>, //이 repository가 DB에 있던 정보를 불러온 것
   ) {}
 
   async create(createDto: CreateDto): Promise<ResponseBody> {
-    const meetingInDto: Meeting = this.meetingsRepository.create(createDto);
-    await this.meetingsRepository.save(meetingInDto);
-    return SuccessResponse(RESPONSE_CODE[2000], { id: meetingInDto.id });
+    //여기에 있는 createDto가 클라로부터 body로 넘어온 것
+    const teamInDto: Team = await this.teamsRepository.findOne({
+      // teamInDto는 teams 테이블에서 id가 body로 입력받은 teamId와 일치한 Team 전체가 저장된다.
+      where: { id: createDto.teamId },
+    });
+
+    if (teamInDto == undefined) {
+      throw new Exception(RESPONSE_CODE[24040], null);
+    }
+
+    const meeting: Meeting = this.meetingsRepository.create();
+    meeting.team = teamInDto;
+    meeting.name = createDto.name;
+    meeting.duration = createDto.duration;
+    meeting.location = createDto.location;
+
+    await this.meetingsRepository.save(meeting);
+    return SuccessResponse(RESPONSE_CODE[2000], { id: meeting.id });
   }
 }
